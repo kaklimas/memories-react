352aee4dd69ae348355732b8b9b7c2a04cef0c2a
kaklimas <klimaskacper@interia.pl>
2022-05-11 08:39:09 +0200

    fixing post method

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -2,7 +2,6 @@ import React, {useState} from 'react'
 import axios from "axios";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import styled from 'styled-components'
-import {logDOM} from "@testing-library/react";
 
 const postURL = 'http://localhost:4000/app/newpost'
 const getURL = 'http://localhost:4000/app/posts'
@@ -24,31 +23,36 @@ function Form() {
     const [message, setMessage] = useState('');
     const [tags, setTags] = useState('')
     const [img, setImg] = useState('');
-    const [nextID, setNextID] = useState(0);
-
-    const getNextID = async () => {
-        let best = 0
-        await axios.get(getURL)
-            .then()
-            .catch((err) => console.log(err))
-
-        // console.log('method', res)
-    }
 
     const handleSubmit = async (event) => {
         event.preventDefault()
+        await axios.get(getURL)
+            .then((res) => {
+                const resDataLength = res.data.length;
+                let maxID = -1
+                res.data.forEach(card => {
+                    if (maxID < card.id) maxID = card.id
+                })
+                maxID++;
 
+                const data = {
+                    "id": maxID,
+                    "title": title,
+                    "message": message,
+                    "creator": creator,
+                    "tags": tags,
+                    "selectedFile": img
+                }
+                data.id > -1 ?
+                    axios.post(postURL, data)
+                        .then((res) => console.log(res))
+                        .catch((err) => console.log(err))
+                    : console.log("data id == 0, ERR")
+            })
+            .catch((err) => console.log(err))
         // getNextID()
         //     .then(async () => {
-        //         console.log('submit body', nextID)
-        //         const data = {
-        //             "id": nextID,
-        //             "title": title,
-        //             "message": message,
-        //             "creator": creator,
-        //             "tags": tags,
-        //             "selectedFile": img
-        //         }
+        //
         //         nextID > -1 ?
         //         await axios.post(postURL, data)
         //             .then((res) => console.log(res))
@@ -56,14 +60,14 @@ function Form() {
         //             : console.log('ID == -1!!')
         //     })
         //     .catch((err) => console.log(err))
+        cleanUpInputs()
     }
-    const handleClear = () => {
+    const cleanUpInputs = () => {
         setImg('');
         setTags('');
         setCreator('');
         setMessage('');
         setTitle('');
-
     }
 
 
@@ -73,26 +77,33 @@ function Form() {
             </div>
             <form onSubmit={handleSubmit}>
                 <div className='mb-3'>
-                    <input required value={creator} type="text" className="form-control" id="creatorName" placeholder='Creator' onChange={(event) => setCreator(event.target.value)}></input>
+                    <input required value={creator} type="text" className="form-control" id="creatorName"
+                           placeholder='Creator' onChange={(event) => setCreator(event.target.value)}></input>
                 </div>
                 <div className='mb-3'>
-                    <input required value={title} type="text" className="form-control" id="title" placeholder='Title' onChange={(event) => setTitle(event.target.value)}></input>
+                    <input required value={title} type="text" className="form-control" id="title" placeholder='Title'
+                           onChange={(event) => setTitle(event.target.value)}></input>
                 </div>
                 <div className="mb-3">
-                    <textarea required value={message} className="form-control" id="message" rows="3" placeholder='Message' onChange={(event) => setMessage(event.target.value)}
+                    <textarea required value={message} className="form-control" id="message" rows="3"
+                              placeholder='Message' onChange={(event) => setMessage(event.target.value)}
                               style={{resize: 'none'}}></textarea>
                 </div>
                 <div className='mb-3'>
-                    <input required type="text" value={tags} className="form-control" id="tags" onChange={(event) => setTags(event.target.value)}
+                    <input required type="text" value={tags} className="form-control" id="tags"
+                           onChange={(event) => setTags(event.target.value)}
                            placeholder='Tags (separated by commas)'></input>
                 </div>
                 <div className='mb-3'>
-                    <input required type="file" name="file" value={img} onChange={(event) => setImg(event.target.value)}/>
+                    <input required type="file" name="file" value={img}
+                           onChange={(event) => setImg(event.target.value)}/>
                 </div>
 
 
                 <button type="submit" className="btn btn-primary" style={{width: '100%'}}>Submit</button>
-                <button className="btn btn-danger" style={{width: '100%', marginTop: 10}} onClick={handleClear}>Clear</button>
+                <button className="btn btn-danger" style={{width: '100%', marginTop: 10}}
+                        onClick={cleanUpInputs}>Clear
+                </button>
 
             </form>
         </FormDiv>
